61c2716c4ea0ce99fd7b320cb0ec91ce8b9e5521
alitechproed <acan.techproed@gmail.com>
2021-01-29 15:00:37 +0300

    15 soru

---
+++ src/codingQuestions/Q_12_VovelorConsonant2.java
@@ -0,0 +1,30 @@
+package codingQuestions;
+
+import java.util.Scanner;
+
+public class Q_12_VovelorConsonant2 {
+    public static void main(String[] args) {
+        //Check whether an alphabet is Vowel or Consonant
+        //a,e,i,o,u
+
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("give me a char please");
+        char ch = scan.next().toUpperCase().charAt(0);
+
+        switch (ch) {
+            case 'a':
+            case 'e':
+            case 'i':
+            case 'o':
+            case 'u':
+                System.out.println(ch + " is vovel");
+                break;
+            default:
+                System.out.println(ch + " is consonant");
+                break;
+        }
+
+
+    }
+}
---
+++ src/codingQuestions/Q_15_LeapYear2.java
@@ -0,0 +1,23 @@
+package codingQuestions;
+
+import java.util.Scanner;
+
+public class Q_15_LeapYear2 {
+
+    public static void main(String[] args) {
+
+
+        //4 e bolunmezse artik degil  +
+        //4 e bolunurse ve 100 e BOLUNMEZ ISE  ARTIK YIL
+        //4 e, 100 e ve 400 e de bolunurse ARTIK YIL
+
+        Scanner scan=new Scanner(System.in);
+        System.out.println("give me a year for check");
+        int year=scan.nextInt();
+
+            String a=((year % 4 == 0) ? ((year % 100 == 0) ? ((year % 400 == 0) ? "artikYil" : "artik Yil degil")
+                    : "artik Yil" ): "artik Yil degil");
+        System.out.println(a);
+
+    }
+}
