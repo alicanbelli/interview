d4472efa7f4beeb5198ae584bb6b2a73fcd5cf83
alitechproed <acan.techproed@gmail.com>
2021-01-29 10:25:29 +0300

    6 soru

---
+++ src/codingQuestions/Q_01_Integer.java
@@ -0,0 +1,18 @@
+package codingQuestions;
+
+import java.util.Scanner;
+
+public class Q_01_Integer {
+
+    public static void main(String[] args) {
+
+
+        //Print an integer getting input from user
+
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Please enter number");
+        int number=scan.nextInt();
+        System.out.println(number+ " numarasini girdiniz... Well done");
+    }
+}
---
+++ src/codingQuestions/Q_06_AsciiNumbers.java
@@ -0,0 +1,19 @@
+package codingQuestions;
+
+public class Q_06_AsciiNumbers {
+
+    public static void main(String[] args) {
+
+        //Find ASCII of Characters
+
+        //1 way
+        char num= 'z';
+        int asciiOfZ=num;
+        System.out.println(asciiOfZ);
+
+        //2.way
+        int NEW_asciiOfZ=(int)num;
+        System.out.println(NEW_asciiOfZ);
+
+    }
+}
