760d36e89fabbd57926ecfa2b8165238a2daf613
alitechproed <acan.techproed@gmail.com>
2021-02-03 12:13:57 +0300

    a

--- src/codingQuestions_2/Q_11_PrimeNumber.java
+++ src/codingQuestions_2/Q_11_PrimeNumber.java
@@ -6,21 +6,16 @@ public class Q_11_PrimeNumber {
     public static void main(String[] args) {
         //Prime Numbers/Asal SAyilar
         //sadece kendisine ve 1 e bolunen sayilara asal sayi denir.
-
-
         Scanner scan=new Scanner(System.in);
         System.out.println("Give me a number for check Prime or not?");
         int num=scan.nextInt();
-
         int counter=0;
-
       if (num<=1){
           System.out.println("Buyuk bir sayi giriniz...");
       }else{
           for (int i = 1; i <=num ; i++) {
               if (num%i==0){
                   counter++;
-
               }
           }
           if (counter==2){
@@ -28,12 +23,6 @@ public class Q_11_PrimeNumber {
           }else{
               System.out.println("Sayi asal degildir...");
           }
-
-
-
       }
-
-
-
     }
 }
